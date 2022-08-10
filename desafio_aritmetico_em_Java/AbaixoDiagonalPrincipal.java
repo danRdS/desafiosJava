ab6b9828b347695a00f3023f29c4ca55c08b9f4d
danRdS <danielrdsilva@gmail.com>
2022-08-10 16:29:25 -0300

    Correção do primeiro código

--- desafio_aritmetico_em_Java/AbaixoDiagonalPrincipal.java
+++ desafio_aritmetico_em_Java/AbaixoDiagonalPrincipal.java
@@ -31,16 +31,16 @@ public class AbaixoDiagonalPrincipal {
 		char O = sc.next().toUpperCase().charAt(0);
 		System.out.println("\n" + O);
 		
-		double M[][] = new double[3][3];
+		double M[][] = new double[12][12];
 		
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
+		for(int i=0; i<12; i++) {
+			for(int j=0; j<12; j++) {
 				M[i][j] = sc.nextDouble();
 			}
 		}
 		double soma = 0.0;
 		
-		for(int i=1; i<3; i++) {
+		for(int i=1; i<12; i++) {
 			for(int j=0; j<i; j++) {
 				soma += M[i][j];
 			}
